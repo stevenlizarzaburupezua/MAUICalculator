866d29762769c71d18aee6812deb6030054d7f52
Steven Lizarzaburu Pezúa <lizarzaburupezuasteven@gmail.com>
2023-04-09 21:57:39 -0500

    Creando los comandos para las operaciones

---
+++ MAUICalculator/MVVM/ViewModels/CalcViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MAUICalculator.MVVM.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class CalcViewModel
+    {
+        public string Formula { get; set; }
+
+        public string Result { get; set; } = "0";
+
+        public ICommand OperationCommand => new Command((number) => { Formula += number; });
+    }
+}
