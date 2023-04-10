2e3b934e8c0fda2ba8ace0d4737847744f2045c0
Steven Lizarzaburu Pezúa <lizarzaburupezuasteven@gmail.com>
2023-04-09 23:02:08 -0500

    Finalización del Proyecto

--- MAUICalculator/MVVM/ViewModels/CalcViewModel.cs
+++ MAUICalculator/MVVM/ViewModels/CalcViewModel.cs
@@ -1,4 +1,5 @@
-﻿using PropertyChanged;
+﻿using Dangl.Calculator;
+using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,5 +17,30 @@ namespace MAUICalculator.MVVM.ViewModels
         public string Result { get; set; } = "0";
 
         public ICommand OperationCommand => new Command((number) => { Formula += number; });
+
+        public ICommand ResetCommand => new Command(() => { Result = "0"; Formula = ""; });
+
+        public ICommand BackSpaceCommand => new Command(() => 
+        
+        {
+            if (Formula.Length > 0)
+            {
+                Formula = Formula.Substring(0, Formula.Length - 1);
+            }        
+        });
+
+        public ICommand CalculateCommand => new Command(() =>
+
+        {
+            if (Formula.Length == 0)
+            {
+                return;
+            }
+
+            var calculation = Calculator.Calculate(Formula);
+            Result = calculation.Result.ToString();
+
+        });
+
     }
 }
